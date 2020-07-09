bd5220089ae2bd9192596d3f98708b4ebec68ed3
Kaitlyn Crawford <kaitlyn@praekelt.com>
2020-07-09 12:34:26 +0200

    Remove commenting tests since this app doesn't have comments

--- who/tests/test_views.py
+++ who/tests/test_views.py
@@ -13,7 +13,6 @@ from rest_framework import status
 
 from molo.core.models import Main, Languages, SiteLanguageRelation
 from molo.core.tests.base import MoloTestCaseMixin
-from molo.commenting.models import MoloComment
 
 
 class ViewsTestCase(MoloTestCaseMixin, TestCase):
@@ -31,31 +30,6 @@ class ViewsTestCase(MoloTestCaseMixin, TestCase):
         self.content_type = ContentType.objects.get_for_model(self.user)
         self.client = Client()
 
-    def mk_comment(self, comment):
-        return MoloComment.objects.create(
-            content_type=self.content_type,
-            object_pk=self.user.pk,
-            content_object=self.user,
-            site=Site.objects.get_current(),
-            user=self.user,
-            comment=comment,
-            submit_date=datetime.now())
-
-    def test_reporting_comment(self):
-        comment = self.mk_comment('the comment')
-
-        response = self.client.get(reverse(
-            'molo.commenting:molo-comments-report', args=(comment.pk,)))
-
-        self.assertEqual(
-            response['location'],
-            '/profiles/login/?next=/commenting/molo/report/1/')
-
-        self.client.login(username='test', password='test')
-
-        response = self.client.get(reverse(
-            'molo.commenting:molo-comments-report', args=(comment.pk,)))
-        self.assertEqual(response['location'], '/cr/4/1/#c1')
 
     def test_health_no_interface_set(self):
         """
