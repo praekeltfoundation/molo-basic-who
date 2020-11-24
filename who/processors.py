86aa967c9ae6fcbe3be5f5042040f1e3621095b5
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-11-24 09:05:53 +0200

    add language_code context processor

--- who/processors.py
+++ who/processors.py
@@ -1,8 +1,10 @@
 from django.conf import settings
+from django.utils.translation import LANGUAGE_SESSION_KEY
 
 
 def compress_settings(request):
     return {
         'STATIC_URL': settings.STATIC_URL,
-        'ENV': settings.ENV
+        'ENV': settings.ENV,
+        'LANGUAGE_CODE': request.session.get(LANGUAGE_SESSION_KEY),
     }
